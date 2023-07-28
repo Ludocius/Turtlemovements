e7a8fb7cf254da4e9de56e7585adb1d130deb6fd
Luis Fernando Pinzon Marroquin <59232095+Ludocius@users.noreply.github.com>
2023-07-28 14:24:44 -0500

    Starting
    Project start

---
+++ src/test/java/org/turtlemovements/GeneratePrefixedMapTest.java
@@ -0,0 +1,32 @@
+package org.turtlemovements;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class GeneratePrefixedMapTest {
+
+    final int ROWS = 10;
+    final int COLUMNS = 10;
+    int[][] map = new int[ROWS][COLUMNS];
+    GeneratePrefixedMap generatePrefixedMap;
+
+    @BeforeEach
+    void startup() {
+        generatePrefixedMap = new GeneratePrefixedMap(map);
+    }
+
+    @Test
+    void doesTheMapHaveTheColumnsDefined() {
+        int columnsSize = generatePrefixedMap.getColumns();
+        Assertions.assertEquals(COLUMNS, columnsSize);
+    }
+
+    @Test
+    void doesTheMapHaveTheRowsDefined() {
+        int rowsSize = generatePrefixedMap.getRows();
+        Assertions.assertEquals(ROWS, rowsSize);
+    }
+
+}
