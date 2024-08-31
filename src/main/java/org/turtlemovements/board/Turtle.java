338ee0ca9cdf245dab1c8e1fa48c543907c4a3b5
Luis Fernando Pinzon Marroquin <59232095+Ludocius@users.noreply.github.com>
2024-08-31 17:25:15 -0500

    fix: refactor made on Turtle

--- src/main/java/org/turtlemovements/board/Turtle.java
+++ src/main/java/org/turtlemovements/board/Turtle.java
@@ -3,17 +3,14 @@ package org.turtlemovements.board;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@RequiredArgsConstructor
 public class Turtle {
     private int positionInX;
     private int positionInY;
-
-    public Turtle(int initialPositionInX, int initialPositionInY) {
-        this.positionInX = initialPositionInX;
-        this.positionInY = initialPositionInY;
-    }
 }
